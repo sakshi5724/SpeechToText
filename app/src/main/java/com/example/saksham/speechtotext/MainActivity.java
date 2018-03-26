a9a1c3034ab2e991326f9853679985ff7c432572
Sakshi5724 <sakshi.s.a@outlook.com>
2018-03-26 21:28:53 +0530

    test

---
+++ app/src/main/java/com/example/saksham/speechtotext/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.saksham.speechtotext;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.tv);
+    }
+
+    public void getSpeechInput(View view) {
+        Intent intent= new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        if(intent.resolveActivity(getPackageManager())!=null) {
+            startActivityForResult(intent, 10);
+        }else{
+            Toast.makeText(this, "your device don't support speech", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 10:
+                if(resultCode==RESULT_OK&&data!=null){
+                    ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textView.setText(result.get(0));
+                }
+                break;
+        }
+    }
+}
+
